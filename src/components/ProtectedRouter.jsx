2db22be492846600b003aac0241065af8eba8a0a
Ahmed-abdisalaam-ahmed <shihabeldin70@gmail.com>
2026-01-12 18:45:03 +0300

    feat: add Producted Router

--- src/components/ProtectedRouter.jsx
+++ src/components/ProtectedRouter.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import { Navigate } from "react-router";
+import { useAuth } from "../contexts/AuthContext";
 
-export const ProtectedRouter = () => {
-  return (
-    <div>ProtectedRouter</div>
-  )
+export default function ProtectedRoute({children, redirectTo = "/signin"}){
+
+    const {isLoading, isLoggedIn} = useAuth();
+
+    if(isLoading){
+        return(
+            <div className="min-h-screen flex items-center justify-center">
+                <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-orange-500"></div>
+            </div>
+        )
+    }
+
+    if(!isLoggedIn){
+        return <Navigate to={redirectTo} replace />
+    }
+
+    return children;
 }
